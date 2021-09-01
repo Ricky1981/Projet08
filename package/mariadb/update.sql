aed40bbf747a60668ed01745430d36247e608cde
vagrant <vagrant@projet08>
2021-09-01 21:37:43 +0200

    Update IP / URI

--- package/mariadb/update.sql
+++ package/mariadb/update.sql
@@ -1,4 +1,4 @@
 update prestashop.ps_configuration set value='172.18.0.2' where name in ('PS_SHOP_DOMAIN','PS_SHOP_DOMAIN_SSL');
-update prestashop.ps_shop_url set domain = 'localhost:8083' where id_shop_url=1;
-update prestashop.ps_shop_url set domain_ssl = 'localhost:8083' where id_shop_url=1;
+update prestashop.ps_shop_url set domain = '192.168.10.8:8083' where id_shop_url=1;
+update prestashop.ps_shop_url set domain_ssl = '192.168.10.8:8083' where id_shop_url=1;
 update prestashop.ps_shop_url set physical_uri = '/' where id_shop_url=1;
