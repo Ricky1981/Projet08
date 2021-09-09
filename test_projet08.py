aeefe53bdd167c637999fc5321e0c692338a10e1
vagrant <vagrant@projet08>
2021-09-09 11:31:10 +0200

    pytest

--- test_projet08.py
+++ test_projet08.py
@@ -9,29 +9,25 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 
-#class TestTest():
-#  def setup_method(self, method):
-driver = webdriver.Chrome()
-vars = {}
+class TestTest():
+  def setup_method(self, method):
+    options = Options()
+    options.headless = True
+    self.driver = webdriver.Chrome(options=options)
+    self.vars = {}
   
-#  def teardown_method(self, method):
-driver.quit()
+  def teardown_method(self, method):
+    self.driver.quit()
   
-#  def test_test(self):
-driver.get("http://192.168.10.8:8083/index.php")
-driver.set_window_size(1886, 1016)
-driver.find_element(By.CSS_SELECTOR, "a > .hidden-sm-down").click()
-driver.find_element(By.NAME, "email").click()
-driver.find_element(By.NAME, "email").send_keys("test@test.fr")
-driver.find_element(By.CSS_SELECTOR, ".form-group:nth-child(3) > .form-control-label").click()
-elements = driver.find_elements(By.CSS_SELECTOR, ".form-group:nth-child(3) > .form-control-label")
-assert len(elements) > 0
-driver.find_element(By.NAME, "password").click()
-driver.find_element(By.NAME, "password").send_keys("Jfm2000m!")
-driver.find_element(By.ID, "submit-login").click()
-elements = find_elements(By.CSS_SELECTOR, ".logout")
-assert len(elements) > 0
-driver.find_element(By.CSS_SELECTOR, ".logout").click()
-driver.close()
-  
+  def test_test(self):
+    self.driver.get("http://192.168.10.8:8083/index.php")
+    self.driver.find_element(By.CSS_SELECTOR, "a > .hidden-sm-down").click()
+    self.driver.find_element(By.NAME, "email").click()
+    self.driver.find_element(By.NAME, "email").send_keys("test@test.fr")
+    self.driver.find_element(By.NAME, "password").click()
+    self.driver.find_element(By.NAME, "password").send_keys("Jfm2000m!")
+    self.driver.find_element(By.ID, "submit-login").click()
+    self.driver.find_element(By.CSS_SELECTOR, "#identity-link .material-icons").click()
+    self.driver.find_element(By.CSS_SELECTOR, ".logout").click()
